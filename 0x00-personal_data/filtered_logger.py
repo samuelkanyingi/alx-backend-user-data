fb8d5f6d8d328303bbc93f3d7a510ffb14f13a42
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-06 05:25:47 +0000

    user data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+filtered_logger module
+"""
+
+import re
+import logging
+from typing import List, Tuple
+
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "ssn", "password", "date_of_birth")
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Returns the log message with specified fields obfuscated.
+
+    Args:
+        fields (List[str]): Fields to obfuscate.
+        redaction (str): String to replace the field values with.
+        message (str): Log message.
+        separator (str): Character separating the fields in the log message.
+
+    Returns:
+        str: Obfuscated log message.
+    """
+    pattern = '|'.join([f'{field}=.*?{separator}' for field in fields])
+    return re.sub(pattern, lambda m: f'{m.group(0).split("=")[0]}={redaction}{separator}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger with the specified configuration.
+
+    Returns:
+        logging.Logger: Configured logger.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+
+    logger.addHandler(handler)
+
+    return logger
